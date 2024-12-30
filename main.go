3360f4ae07da4fdfeaeb1f81edf7ec040e6ca1dc
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-12-30 15:09:57 -0800

    first attempt at converting image to ascii

--- main.go
+++ main.go
@@ -2,24 +2,75 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"image/png"
 	"os"
+	"strings"
+	"sync"
+
+	"github.com/nfnt/resize"
 )
 
 func main() {
 	file, err := os.Open("static/sephiroth.png")
 	if err != nil {
-		fmt.Println("Error opening image:", err)
+		fmt.Printf("Failed to open image: %v\n", err)
 		return
 	}
 	defer file.Close()
 
 	img, err := png.Decode(file)
 	if err != nil {
-		fmt.Println("Error decoding image:", err)
+		fmt.Printf("Failed to decode image: %v\n", err)
 		return
 	}
+
+	// Resize the image
+	img = resize.Resize(400, 0, img, resize.Lanczos3)
+
+	// Convert to ASCII
+	ascii := convertToASCII(img)
+	fmt.Println(ascii)
+}
+
+func convertToASCII(img image.Image) string {
+	// asciiChars := "@#%XO+=:-. "
+	asciiChars := " .-:=+OX%#@"
+	lookup := createASCIILookup(asciiChars)
 	bounds := img.Bounds()
-	fmt.Println("Width:", bounds.Dx())
-	fmt.Println("Height:", bounds.Dy())
+	height := bounds.Dy()
+	width := bounds.Dx()
+
+	result := make([]string, height)
+	var wg sync.WaitGroup
+
+	for y := 0; y < height; y++ {
+		wg.Add(1)
+		go func(y int) {
+			defer wg.Done()
+			line := ""
+			for x := 0; x < width; x++ {
+				color := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+				line += string(lookup[color.Y])
+			}
+			result[y] = line
+		}(y)
+	}
+
+	wg.Wait()
+	return joinLines(result)
+}
+
+func createASCIILookup(asciiChars string) []rune {
+	lookup := make([]rune, 256)
+	for i := 0; i < 256; i++ {
+		index := i * (len(asciiChars) - 1) / 255
+		lookup[i] = rune(asciiChars[index])
+	}
+	return lookup
+}
+
+func joinLines(lines []string) string {
+	return strings.Join(lines, "\n")
 }
