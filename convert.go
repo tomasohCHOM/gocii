2dbb01ca214f8dd9cc9bef211fa0ef3cb668c020
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-01-16 18:20:49 -0800

    return byte array instead of string

---
+++ convert.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"image"
+	"image/color"
+)
+
+const DEPTH = 256
+
+func asciiLookup(asciiChars string) []byte {
+	lookup := make([]byte, DEPTH)
+	for i := range DEPTH {
+		idx := i * (len(asciiChars) - 1) / (DEPTH - 1)
+		lookup[i] = byte(asciiChars[idx])
+	}
+
+	return lookup
+}
+
+func ImageToAscii(img image.Image, invert bool) []byte {
+	asciiChars := "@#%XO+=:-. "
+	if invert {
+		asciiChars = " .-:=+OX%#@"
+	}
+	lookup := asciiLookup(asciiChars)
+
+	bounds := img.Bounds()
+	M, N := bounds.Dy(), bounds.Dx()
+	result := make([]byte, 0, M*N+N)
+
+	for r := range M {
+		for c := range N {
+			color := color.GrayModel.Convert(img.At(c, r)).(color.Gray)
+			result = append(result, lookup[color.Y])
+		}
+		result = append(result, '\n')
+	}
+	return result
+}
--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
-	"image/png"
+	_ "image/png"
 	"os"
-	"strings"
 
 	"github.com/nfnt/resize"
 )
@@ -19,52 +17,13 @@ func main() {
 	}
 	defer file.Close()
 
-	img, err := png.Decode(file)
+	img, _, err := image.Decode(file)
 	if err != nil {
 		fmt.Printf("Failed to decode image: %v\n", err)
 		return
 	}
+	img = resize.Resize(800, 400, img, resize.Lanczos3)
 
-	// Resize the image
-	img = resize.Resize(400, 200, img, resize.Lanczos3)
-
-	// Convert to ASCII
-	ascii := convertToASCII(img)
-	fmt.Println(strings.Index(ascii, "\n"))
-	fmt.Println(len(ascii))
-	fmt.Println(ascii)
-}
-
-func convertToASCII(img image.Image) string {
-	asciiChars := " .-:=+OX%#@"
-	lookup := createASCIILookup(asciiChars)
-	bounds := img.Bounds()
-	height := bounds.Dy()
-	width := bounds.Dx()
-
-	result := make([]string, height)
-
-	for y := range height {
-		var line strings.Builder
-		for x := range width {
-			color := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
-			line.WriteString(string(lookup[color.Y]))
-		}
-		result[y] = line.String()
-	}
-
-	return joinLines(result)
-}
-
-func createASCIILookup(asciiChars string) []rune {
-	lookup := make([]rune, 256)
-	for i := range 256 {
-		index := i * (len(asciiChars) - 1) / 255
-		lookup[i] = rune(asciiChars[index])
-	}
-	return lookup
-}
-
-func joinLines(lines []string) string {
-	return strings.Join(lines, "\n")
+	ascii := ImageToAscii(img, true)
+	fmt.Println(string(ascii))
 }
